db393b5f5386e44b3633cd8900e8feb06973c44a
amelv-03 <aiden.melv03@gmail.com>
2024-09-18 21:56:03 +0000

    keep going

--- src/app.js
+++ src/app.js
@@ -7,5 +7,20 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  btn.addEventListener("click", function() {
+    generateCard();
+  });
+};
+
+
+const generateCard = () => {
+  let suit = ["♦", "♣", "♥", "♠"];
+  let rank = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"];
+
+  let randomSuit = suit[Math.floor(Math.random() * suit.length)];
+  let randomRank = rank[Math.floor(Math.random() * rank.length)];
+
+  if (randomSuit === "♦","♥"){
+    
+  }
 };
