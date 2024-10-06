5822c4991daceb6b06677aa2c800d13478233c3d
amelv-03 <aiden.melv03@gmail.com>
2024-10-06 22:28:23 +0000

    finished

--- src/app.js
+++ src/app.js
@@ -4,7 +4,11 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
-
+import { bottom } from "@popperjs/core";
+let btn = document.querySelector(".btn");
+let topsuit = document.querySelector(".top");
+let bottomsuit = document.querySelector(".bottom");
+let rank = document.querySelector(".rank");
 window.onload = function() {
   //write your code here
   btn.addEventListener("click", function() {
@@ -12,20 +16,37 @@ window.onload = function() {
   });
 };
 
-
 const generateCard = () => {
   let suit = ["♦", "♣", "♥", "♠"];
-  let rank = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"];
+  let ranks = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
 
   let randomSuit = suit[Math.floor(Math.random() * suit.length)];
-  let randomRank = rank[Math.floor(Math.random() * rank.length)];
+  let randomRank = ranks[Math.floor(Math.random() * ranks.length)];
 
-  if (randomSuit === "♦","♥"){
-    btn.style.color = "red";
+  if (randomSuit == "♥" || randomSuit == "♦") {
+    topsuit.style.color = "red";
+    bottomsuit.style.color = "red";
     rank.style.color = "red";
-  }
-  else {
-    btn.style.color = "black";
+  } else {
+    topsuit.style.color = "black";
+    bottomsuit.style.color = "black";
     rank.style.color = "black";
   }
+  topsuit.innerHTML = randomSuit;
+  bottomsuit.innerHTML = randomSuit;
+  rank.innerHTML = randomRank;
 };
